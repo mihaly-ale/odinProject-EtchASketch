17b36f6f9ffe971a617324f85d50f822a0bc8167
mihaly-ale <105678319+mihaly-ale@users.github.com>
2023-09-24 22:43:28 +0200

    Get grid size from customer input
    add gridSize named CSS custom property to container class
    gridSize update from input

--- app.js
+++ app.js
@@ -3,20 +3,28 @@ const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
+// getting grid sizes from customer input
+let gridSize = 0;
+const gridSizeInput = document.createElement("input");
+document.body.insertBefore(gridSizeInput, container);
+gridSizeInput.addEventListener('keyup', (e) => {  
+   gridSize = parseIint(e.target.value);
+   container.style.setProperty("--gridSize", gridSize)
+});
 
-// add boxes
-let verticalBoxesCount = 16;
-let horizontalBoxesCount = 16;
 
-function drawGrid(verticalBoxesCount, horizontalBoxesCount) {
-    let totalBoxesCount = verticalBoxesCount * horizontalBoxesCount;
+// draw the grid
+function drawGrid(gridSize) {
+  console.log(gridSize)
+    let totalBoxesCount = gridSize * gridSize;
     for (let i = 1; i <= totalBoxesCount; i++) {
         const box = document.createElement("div");
+        box.innerText = i;
         box.classList.add("box");
         container.appendChild(box);
     }
 }
 
-drawGrid(verticalBoxesCount, horizontalBoxesCount);
+drawGrid(gridSize);
 
 
