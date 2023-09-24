13c0b9b35578456146a3e569c089f26e616a3255
mihaly-ale <105678319+mihaly-ale@users.github.com>
2023-09-25 00:13:14 +0200

    add button for drawing the grid
    add button and event listener to button to invoke drawGrid function

--- app.js
+++ app.js
@@ -14,12 +14,21 @@ gridSizeInput.addEventListener('keyup', (e) => {
    container.style.setProperty("--gridSize", gridSize)
 });
 
+// label for the input element
 const labelForInput =  document.createElement("label");
 labelForInput.innerHTML= "Type in your grid size:";
 labelForInput.setAttribute("for", "gridSizeInput")
 document.body.insertBefore(labelForInput, gridSizeInput);
 
 
+//add button to draw the grid
+const drawGridBtn = document.createElement("button");
+drawGridBtn.innerText = "draw grid";
+drawGridBtn.classList.add("make-grid-button")
+document.body.insertBefore(drawGridBtn, container);
+drawGridBtn.addEventListener('click', () =>{
+  drawGrid(gridSize)
+});
 
 // draw the grid
 function drawGrid(gridSize) {
@@ -33,6 +42,6 @@ function drawGrid(gridSize) {
     }
 }
 
-drawGrid(gridSize);
+
 
 
