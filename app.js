44d06362d746c20675c793d32dbb4b7ba572da91
mihaly-ale <105678319+mihaly-ale@users.github.com>
2023-09-26 10:18:17 +0200

    create basic layout
    -add h1 element
    -add game container with child sections:
        -inputSection
        -gameSettings
        -gameGrid
    -gameGrid change from div to section
    -further divide inputSection into:
        -labelForInput
        -gridSizeInput
        -drawGridBtn

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
 //create base layout
-// h1 DONE
-// grid draw (label, inout, button)
+
 //game (control, gameGrid)
 const h1 = document.createElement("h1");
 h1.textContent = "Etch A Sketch";
 h1.classList.add("h1");
 document.body.appendChild(h1);
 
+//create layout for game 
+const gameContainer = document.createElement("div");
+gameContainer.classList.add("game-container");
+document.body.appendChild(gameContainer);
 
 
-// add container for boxes
-const gameGrid = document.createElement("div");
-gameGrid.classList.add("game-grid");
-document.body.appendChild(gameGrid);
+const inputSection =  document.createElement("section");
+inputSection.classList.add("input-section");
+gameContainer.appendChild(inputSection);
+// label for the input element
+const labelForInput =  document.createElement("label");
+labelForInput.innerHTML= "Type in your grid size:";
+labelForInput.setAttribute("for", "gridSizeInput")
+inputSection.appendChild(labelForInput);
 
 // getting grid sizes from customer input
 let gridSize = 0;
 const gridSizeInput = document.createElement("input");
 gridSizeInput.classList.add("input-grid-size")
 gridSizeInput.setAttribute("id", "gridSizeInput")
-document.body.insertBefore(gridSizeInput, gameGrid);
-gridSizeInput.addEventListener('keyup', (e) => {  
-   gridSize = parseInt(e.target.value);
-   gameGrid.style.setProperty("--gridSize", gridSize)
-});
-
-// label for the input element
-const labelForInput =  document.createElement("label");
-labelForInput.innerHTML= "Type in your grid size:";
-labelForInput.setAttribute("for", "gridSizeInput")
-document.body.insertBefore(labelForInput, gridSizeInput);
+inputSection.appendChild(gridSizeInput);
+// gridSizeInput.addEventListener('keyup', (e) => {  
+//    gridSize = parseInt(e.target.value);
+//    gameGrid.style.setProperty("--gridSize", gridSize)
+// }); error, move down
 
 //add button to draw the grid
 const drawGridBtn = document.createElement("button");
 drawGridBtn.innerText = "draw grid";
 drawGridBtn.classList.add("make-grid-button")
-document.body.insertBefore(drawGridBtn, gameGrid);
+inputSection.appendChild(drawGridBtn);
 drawGridBtn.addEventListener('click', () =>{
   drawGrid(gridSize)
 });
+
+
+const gameSettings = document.createElement("section");
+gameSettings.classList.add('game-settings')
+const gameGrid = document.createElement("section");
+gameGrid.classList.add("game-grid");
+gameContainer.appendChild(inputSection);
+gameContainer.appendChild(gameSettings);
+gameContainer.appendChild(gameGrid);
+
+
 
 // draw the grid
 function drawGrid(gridSize) {
