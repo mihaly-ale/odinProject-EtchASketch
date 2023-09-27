48d3b36b85145fbcf3955b1d409eb8eb81a894bd
mihaly-ale <105678319+mihaly-ale@users.github.com>
2023-09-27 18:17:03 +0200

    Refactor drawGrid function
    drawGrid function gets its parameter from the newly added slider (gridSizer)
    gridSlider replace inputSection and its elements(input, label, button)

--- app.js
+++ app.js
@@ -4,10 +4,39 @@ h1.textContent = "Etch A Sketch";
 h1.classList.add("h1");
 document.body.appendChild(h1);
 
-//create inputSection (children: labelForInput, gridSizeInput, drawGridBtn)
-const inputSection = document.createElement("section");
-inputSection.classList.add("input-section");
-document.body.appendChild(inputSection);
+
+const gridSizerContainer =  document.createElement("div");
+document.body.appendChild(gridSizerContainer);
+gridSizerContainer.setAttribute("id", "grid-sizer-container")
+const gridSizer = document.createElement("input");
+gridSizer.setAttribute("type", "range");
+gridSizer.setAttribute("min", "2");
+gridSizer.setAttribute("max", "16");
+gridSizer.setAttribute("id", "grid-sizer");
+gridSizer.setAttribute("value", "8"); 
+const gridSizerLabel =  document.createElement('label');
+gridSizerLabel.setAttribute("for", "grid-sizer");
+gridSizerLabel.setAttribute("id", "grid-sizer-label");
+gridSizerLabel.classList.add("space-between")
+const spanMin = document.createElement("span");
+const spanMax = document.createElement("span");
+gridSizerLabel.appendChild(spanMin);
+spanMin.textContent = "2"
+gridSizerLabel.appendChild(spanMax);
+spanMax.textContent = "16";
+
+gridSizerContainer.appendChild(gridSizer);
+gridSizerContainer.appendChild(gridSizerLabel);
+
+// //add button to draw the grid
+// const drawGridBtn = document.createElement("button");
+// drawGridBtn.innerText = "draw grid";
+// drawGridBtn.classList.add("draw-grid-button")
+// inputSection.appendChild(drawGridBtn);
+// drawGridBtn.addEventListener('click', () => {
+//   const gridSize = gridSizeInput.value;
+//   drawGrid(gridSize);
+// });
 
 //create layout for game (children: inputSection, gameSettings, gameGrid)
 const gameContainer = document.createElement("div");
@@ -15,37 +44,14 @@ gameContainer.classList.add("game-container");
 document.body.appendChild(gameContainer);
 
 
-// label for the input element
-const labelForInput = document.createElement("label");
-labelForInput.innerHTML = "Type in your grid size:";
-labelForInput.setAttribute("for", "gridSizeInput");
-labelForInput.classList.add("label-for-input");
-inputSection.appendChild(labelForInput);
-
-// getting grid sizes from customer input
-const gridSizeInput = document.createElement("input");
-gridSizeInput.classList.add("input-grid-size")
-gridSizeInput.setAttribute("id", "gridSizeInput")
-inputSection.appendChild(gridSizeInput);
-// updating css custom prop for grid
-let cssGridSize = 0;
-gridSizeInput.addEventListener('input', (e) => {
-  cssGridSize = parseInt(e.target.value);
-});
-
-//add button to draw the grid
-const drawGridBtn = document.createElement("button");
-drawGridBtn.innerText = "draw grid";
-drawGridBtn.classList.add("draw-grid-button")
-inputSection.appendChild(drawGridBtn);
-drawGridBtn.addEventListener('click', () => {
-  const gridSize = gridSizeInput.value;
+gridSizer.addEventListener("click", (e) => {
+  const gridSize = e.target.value;
   drawGrid(gridSize);
 });
 
-// draw the grid
-function drawGrid(gridSize) {
-  gameGrid.style.setProperty("--cssGridSize", cssGridSize);
+
+function drawGrid(gridSize) {  
+  gameGrid.style.setProperty("--cssGridSize", gridSize);
   gameGrid.innerHTML = ""; //clear previous grid
   let totalBoxesCount = gridSize * gridSize; 
     for (let i = 1; i <= totalBoxesCount; i++) {
